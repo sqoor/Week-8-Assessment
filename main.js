06e2f94e111ce4b2488f0a696c2579bff91bff04
sqoor <alsqoorAbdullah@gmail.com>
2019-08-04 10:49:37 +0300

    finished the assessment

--- main.js
+++ main.js
@@ -8,24 +8,28 @@ function sum(x,y){
   return x+y
 }
 
+
 2) 
 function consoleReturn(x,y){
   console.log(x)
   return y
 }
 
+
 3) 
 var name="Alex"
 var age=25
 var result="My name is: " + name + "and my age is: " + age
 
+
 4)
-var food="Fried Chicken"
-var color="Blue"
+var food="Fried Chicken";
+var color="Blue";
 var object={
   food:food,
   color:color
 }
+
 
 5)
 var object2={
@@ -33,18 +37,48 @@ var object2={
     return a * b
   }
 }
+
 */
 
-//1) WRITE YOUR CODE UNDER THIS LINE         
+//1) WRITE YOUR CODE UNDER THIS LINE 
 
-//2) WRITE YOUR CODE UNDER THIS LINE         
+let sum = (x, y) => {
+  return x + y;  
+}
 
-//3) WRITE YOUR CODE UNDER THIS LINE         
 
-//4) WRITE YOUR CODE UNDER THIS LINE         
+//2) WRITE YOUR CODE UNDER THIS LINE    
+
+let consoleReturn = (x, y) => {
+  console.log(x);
+  return y;
+} 
+
+
+//3) WRITE YOUR CODE UNDER THIS LINE     
+
+let name = "Alex";
+let age = 25;
+let result = `My name is: ${name} and my age is: ${age}`;
+
+
+//4) WRITE YOUR CODE UNDER THIS LINE    
+
+let food = "Fried Chicken";
+let color = "Blue";
+let object = {
+  food,
+  color
+};
+
 
 //5) WRITE YOUR CODE UNDER THIS LINE         
 
+let object2 = {
+  multi(a, b) {
+    return a * b;
+  }
+}
 
 
 
@@ -74,8 +108,22 @@ Output =>
 
 // WRITE YOUR CODE UNDER THIS LINE
 
+class Computer {
+  constructor(OS, RAM, CPU) {
+    this.os = OS;
+    this.ram = RAM;
+    this.cpu = CPU;
+  }
 
+  doubleRAM() {
+    this.ram *= 2; // this.ram = this.ram * 2;
+    return this.ram;
+  }
+}
 
+let computer1 = new Computer('Windows', 16, 'I7');
+let computer2 = new Computer('Linux', 8, 'I5');
+let computer3 = new Computer('Mac', 4, 'I3');
 
 
 
@@ -86,6 +134,7 @@ please fix the errors inside them
 */
 
 // App Component
+import React, {Component} from 'react';
 import Tasks from './components/Tasks';
 
 export default class App extends Component {
@@ -93,14 +142,19 @@ export default class App extends Component {
     title: 'ELIZABETH GREENE',
     todos: ['eat', 'eat eat', 'eact again']
   };
-  changeTitle() {
-    state.title = 'AGGREGOR ZOLDYCK'
+  changeTitle = () => {
+    // state.title = 'AGGREGOR ZOLDYCK'
+    this.setState({
+      title: 'AGGREGOR ZOLDYCK'
+    });
   }
   render() {
     return (
-      <h1>App Component => state.title</h1>
-      <button onClick={this.changeTitle}>Change Title</button>
-      <Tasks tasks={this.todos} changeTitleFromChild={this.changeTitle} />
+      <>
+        <h1>App Component => state.title</h1>
+        <button onClick={this.changeTitle}>Change Title</button>
+        <Tasks tasks={this.state.todos} changeTitleFromChild={this.changeTitle} />
+      </>
     );
   }
 }
@@ -112,17 +166,21 @@ class Tasks extends Component {
   state = {
     day: "Sat"
   };
-  changeDay() {
-    day = 'Sun'
+  changeDay = () => {
+    // day = 'Sun'
+    this.setState({
+      day: 'Sun'
+    });
   }
-
   render() {
     return (
       <div>
         <h1>Tasks Component => state.day</h1>
         <button onClick={this.changeDay}>Change Tasks State</button>
-        <button onClick={changeTitle}>Change App State</button>
+        <button onClick={this.props.changeTitleFromChild}>Change App State</button>
       </div>
     );
   }
 }
+
+export default Tasks;
